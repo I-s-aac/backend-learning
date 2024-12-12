eb4dc8babf5bb6ae02f247e4c5ab905730dd7ec6
runaround <runaroundontheground@gmail.com>
2024-12-12 10:12:31 -0700

    did an exercise Thu, Dec 12, 2024 10:12:31 AM

--- exercise.js
+++ exercise.js
@@ -1,4 +1,7 @@
 import { EventEmitter } from "node:events";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const rl = readline.createInterface({ input, output });
 
 class Plant extends EventEmitter {
   constructor() {
@@ -9,39 +12,82 @@ class Plant extends EventEmitter {
     this.registerListeners();
   }
   registerListeners() {
-    this.on("error", this.errorListener);
-    this.once("plantSeed", this.plantSeedListener);
-    this.on("water", this.waterListener);
-    this.on("bugAttack", this.bugAttackListener);
-    this.on("harvest", this.harvestListener);
+    this.on("error", this.error);
+    this.once("plantSeed", this.plantSeed);
+    this.on("water", this.water);
+    this.on("bugAttack", this.bugAttack);
+    this.on("harvest", this.harvest);
   }
-  errorListener(err) {
+  error(err) {
     console.error(`an error occured from ${this}: `, err);
   }
-  plantSeedListener() {
+  plantSeed() {
     this.hasBeenPlanted = true;
     this.size = 1;
-    console.log("has been planted");
+    console.log(`seed has been planted, current size is ${this.size}`);
   }
-  waterListener() {
+  water() {
     if (!this.hasBeenPlanted) {
       console.log("must be planted before watering");
     } else {
       this.size += 1;
+      console.log(`plant has grown, current size: ${this.size}`);
     }
   }
-  bugAttackListener() {
+  bugAttack() {
     if (!this.hasBeenPlanted) {
       console.log("bugs can't attack when not planted");
     } else {
       this.size -= 1;
+      console.log(`plant has been attacked, current size: ${this.size}`);
     }
   }
-  harvestListener() {
+  harvest() {
     if (!this.hasBeenPlanted) {
       console.log("can't harvest something that hasn't been planted");
     } else {
-      // do something
+      // log current plant size, remove all listeners
+      console.log(`ur final plant size was ${this.size}`);
+      this.removeAllListeners();
     }
   }
 }
+
+const plant = new Plant();
+
+console.log("welcome to growing a plant");
+console.log("you can enter stuff to grow ur plant");
+
+const run = async () => {
+  const answer = await rl.question(
+    "enter 'plant seed', 'water', 'attack', 'harvest', or 'exit': "
+  );
+
+  if (answer.toLowerCase() === "exit") {
+    console.log("exiting...");
+    rl.close();
+    return;
+  } else {
+    switch (answer.toLowerCase()) {
+      case "plant seed": {
+        plant.emit("plantSeed");
+        break;
+      }
+      case "water": {
+        plant.emit("water");
+        break;
+      }
+      case "attack": {
+        plant.emit("bugAttack");
+        break;
+      }
+      case "harvest": {
+        plant.emit("harvest");
+        break;
+      }
+    }
+    run();
+  }
+};
+
+run();
